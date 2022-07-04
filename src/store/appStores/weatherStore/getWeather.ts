0d165791f496b77afa30eeb63b420a410e74f8c4
misha <melnik_mo@groupbwt.com>
2022-07-04 17:30:40 +0300

    fixed after review

--- src/store/appStores/weatherStore/getWeather.ts
+++
@@ -1,5 +0,0 @@
-import { createSelector } from 'reselect';
-
-const getWeather = createSelector([(state) => state.weatherStore.weather], (data) => data);
-
-export default getWeather;
--- src/store/reducers/indexReducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import weatherStoreReducer from '../appStores/weatherStore/weatherStore';
-
-export default combineReducers({
-  weatherStore: weatherStoreReducer,
-});
